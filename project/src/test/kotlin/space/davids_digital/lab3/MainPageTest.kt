7b2dc3aac10a785fc5af72f7d17e932ec2da441c
david-d25@yandex.ru <david-d25@yandex.ru>
2021-04-26 02:51:35 +0300

    Currency and language tests

--- project/src/test/kotlin/space/davids_digital/lab3/MainPageTest.kt
+++ project/src/test/kotlin/space/davids_digital/lab3/MainPageTest.kt
@@ -1,8 +1,10 @@
 package space.davids_digital.lab3
 
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.openqa.selenium.WebDriver
+import space.davids_digital.lab3.pages.MainPage
 
 private const val MIN_ADULTS_NUM = 1
 private const val MAX_ADULTS_NUM = 30
@@ -18,20 +20,29 @@ class MainPageTest {
     @ProvideWebDrivers
     fun `language changing`(driver: WebDriver) {
         this.driver = driver
-        // todo
+
+        with(MainPage(driver)) {
+            changeLanguage("de")
+            assertEquals("de", getPageLangCode())
+        }
     }
 
     @ParameterizedTest
     @ProvideWebDrivers
     fun `currency changing`(driver: WebDriver) {
         this.driver = driver
-        // todo
+
+        with(MainPage(driver)) {
+            changeCurrency("BGN")
+            assertEquals("BGN", getCurrentCurrency())
+        }
     }
 
     @ParameterizedTest
     @ProvideWebDrivers
     fun `guests filter setting`(driver: WebDriver) {
         this.driver = driver
+
         // todo test adults/children/rooms buttons and label
     }
 
