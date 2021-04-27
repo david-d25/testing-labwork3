a326aae56dc4946459fb6003bf8174a81b3d2ade
david-d25@yandex.ru <david-d25@yandex.ru>
2021-04-28 02:06:51 +0300

    i knew it!

--- project/src/test/kotlin/space/davids_digital/lab3/pages/CommonPage.kt
+++ project/src/test/kotlin/space/davids_digital/lab3/pages/CommonPage.kt
@@ -1,10 +1,18 @@
 package space.davids_digital.lab3.pages
 
+import org.openqa.selenium.TimeoutException
 import org.openqa.selenium.WebDriver
+import org.openqa.selenium.support.ui.WebDriverWait
+import java.time.Duration
 
 open class CommonPage(driver: WebDriver, expectedPageUrlRegex: Regex) {
     init {
-        if (!driver.currentUrl.matches(expectedPageUrlRegex))
+        try {
+            WebDriverWait(driver, 10)
+                .pollingEvery(Duration.ofMillis(100))
+                .until { driver.currentUrl.matches(expectedPageUrlRegex) }
+        } catch (e: TimeoutException) {
             throw IllegalStateException("Expected url to match regex '$expectedPageUrlRegex', but url is '${driver.currentUrl}'")
+        }
     }
 }
